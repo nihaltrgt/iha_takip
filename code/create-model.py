14a714ecd32b650b34d9be3cb82f2f471a0d6484
nihaltrgt <nihaltu50@gmail.com>
2019-09-04 10:07:06 +0300

    name change

--- code/create-model.py
+++ code/create-model.py
@@ -3,7 +3,7 @@ import requests, os, json
 url = "https://app.nanonets.com/api/v2/ObjectDetection/Model/"
 api_key = os.environ.get('NANONETS_API_KEY')
 
-payload = "{\"categories\" : [\"TieFighter\", \"MillenniumFalcon\"]}"
+payload = "{\"categories\" : [\"Plane\"]}"
 headers = {'Content-Type': "application/json",}
 
 response = requests.request("POST", url, headers=headers, auth=requests.auth.HTTPBasicAuth(api_key, ''), data=payload)
